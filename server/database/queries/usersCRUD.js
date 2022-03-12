7d2edc8f38d9153b67250ff2859b12b055d93a44
unknown <stm1998@hotmail.com>
2022-03-13 00:26:51 +0200

    insert into users table query Relates #5

---
+++ server/database/queries/usersCRUD.js
@@ -0,0 +1,11 @@
+const connection = require('../config/connection');
+
+const insertUser = (data) =>{
+    const { username, email, password } = data; 
+    return connection.query({
+        text: `INSERT INTO users (username,email, password) VALUES ($1, $2, $3) RETURNING *; `,
+        values:[username, email, password]
+    });
+} 
+
+module.exports = insertUser;
