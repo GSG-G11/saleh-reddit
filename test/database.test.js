0bf85d6d724808429e8ba9dde356a0351d1dfacb
unknown <stm1998@hotmail.com>
2022-03-13 13:33:05 +0200

    test get user query Relates #20

--- test/database.test.js
+++ test/database.test.js
@@ -1,19 +1,30 @@
 const conniction = require('../server/database/config/connection');
-const { insertUser } =require('../server/database/queries/usersCRUD');
+const { insertUser,getUserByEmail } =require('../server/database/queries/usersCRUD');
 const createUsersTable = require('../server/database/migrations/createUsersTable');
 
-beforeEach(() => createUsersTable());
+// beforeAll(() => createUsersTable());
 
 describe("Test database users table queries", () => {
     test("Insert user into users table", () => {
         const expected = {
           username: "Saleh",
-          email:'stm1998@hot.com',
+          email:'stm19add98@hot.com',
           password: "123123",
         };
         return insertUser(expected).then((data) => {
           expect(data.rows[0].name).toEqual(expected.name);
           expect(data.rows[0].password).toEqual(expected.password);
+        });
+      });
+    test("get user from users table by email", () => {
+        const expected = {
+          username: "saleh",
+          email:'stm1998@hotmail.com',
+          password: "123123",
+        };
+        return getUserByEmail('stm1998@hotmail.com').then((data) => {
+          console.log(data.rows);
+          expect(data.rows[0].username).toEqual(expected.username);
         });
       });
 });
