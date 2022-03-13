d7489ce8668b76142e8185593ead8f5e4c0d56b9
unknown <stm1998@hotmail.com>
2022-03-13 18:10:36 +0200

    create posts taple Relates #27

---
+++ server/database/migrations/tables.sql
@@ -0,0 +1,23 @@
+BEGIN;
+
+DROP TABLE IF EXISTS users, posts CASCADE;
+
+CREATE TABLE users (
+    id serial primary key,
+    username varchar(128) not null,
+    email varchar(56) unique not null,
+    password varchar(200) not null
+);
+
+CREATE TABLE posts (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    content TEXT NOT NULL,
+    community_name VARCHAR(100) NOT NULL,
+    vote INTEGER DEFAULT 0,
+    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    content_type VARCHAR(100),
+    post_date TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+COMMIT;
