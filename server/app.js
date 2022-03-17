63fd493319fcd4525644034886b5174825e070db
unknown <stm1998@hotmail.com>
2022-03-17 18:33:51 +0200

    deploy

--- server/app.js
+++ server/app.js
@@ -9,7 +9,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static('public'));
-app.use(morgan('tiny'));
 app.set('port', process.env.PORT);
 
 app.get('hello', (req, res) => {
