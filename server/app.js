e9846162d76a18caca124764ca798ef20c3febaa
Saleh T. Marouf <54906217+devstm@users.noreply.github.com>
2022-03-17 17:54:55 +0200

    Update app.js

--- server/app.js
+++ server/app.js
@@ -8,7 +8,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static('public'));
-app.use(morgan('tiny'));
 app.set('port', process.env.PORT || 5000);
 
 app.get('hello', (req, res) => {
